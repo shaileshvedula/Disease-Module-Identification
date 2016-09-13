afd6839ddc596ff016bf4458a2aaff7d724c6bd8
Shailesh Vedula <vedulashailesh@gmail.com>
2016-09-12 23:49:50 -0400

    Fixing the commit comment

--- src/main/java/org/svedula/dream/dmi/RunMethod.java
+++ src/main/java/org/svedula/dream/dmi/RunMethod.java
@@ -1,7 +1,32 @@
+/*
+ * ReadInputData.java
+ *
+ * Version 1.0
+ *
+ * Copyright (C) 2016- Shailesh Vedula
+ * Department of Industrial and Operations Engineering
+ * University of Michigan Ann Arbor, MI
+ *
+ * Date: September 9 2016
+ */
+
 package org.svedula.dream.dmi;
 
+import com.google.common.collect.Table;
+import java.io.IOException;
+import java.lang.*;
+
 /**
- * Created by ishan on 9/9/16.
+ * This class contains the main method
  */
+
 public class RunMethod {
+
+    public static void main(String args[]) throws IOException {
+        DataDirectory dataDirectory = new DataDirectory();
+        String[] InputFiles = dataDirectory.GetDataFiles();
+        TransitionMatrix transitionMatrix = new TransitionMatrix(InputFiles[0]);
+        transitionMatrix.MakeTransitionMatrix();
+
+    }
 }
